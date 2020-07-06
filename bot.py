f9bb861dbc2f0f6953e1e6ae77536bc1ae6eb8ca
Dys13xic <campbell.leckie@hotmail.com>
2020-07-06 18:57:20 -0400

    initial commit

---
+++ bot.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("{} has connected to Discord".format(client.user))
+
+client.run(TOKEN)

468161ce1c586d8c9c1b2b917da32b0649dbbe12
Dys13xic <campbell.leckie@hotmail.com>
2020-07-06 23:13:13 -0400

    Bot now responds to private messages, started to configure calling of the enigma executable

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-import os
-import subprocess
 import discord
-from dotenv import load_dotenv
+import subprocess
+import glob
+import shlex
 
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+tokenFile = open("token.txt", 'r')
+token = tokenFile.readline()
 
 client = discord.Client()
 
@@ -14,4 +14,16 @@ client = discord.Client()
 async def on_ready():
     print("{} has connected to Discord".format(client.user))
 
-client.run(TOKEN)
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+
+        if message.channel.type == discord.ChannelType.private:
+            print("Private message received")
+            args = ["./enigma.out"] + shlex.split(message.content)
+            output = subprocess.check_output(args) #glob.glob("./" + "enigma.*")[0], message.content)
+            await message.channel.send((output.decode("utf-8").strip("./")))
+        else:
+            print("Public message receieved")
+
+client.run(token)

5ebe5649d8478513200c040cbf5aabc981ab4961
Dys13xic <campbell.leckie@hotmail.com>
2020-07-08 17:25:53 -0400

    Sends stdout on successful exit of the enigma executable, otherwise sends stderror

--- bot.py
+++ bot.py
@@ -5,11 +5,28 @@ import subprocess
 import glob
 import shlex
 
+EXIT_SUCCESS = 0
+
 # Retrieve the discord bot token
 tokenFile = open("token.txt", 'r')
 token = tokenFile.readline()
 
 client = discord.Client()
+
+async def run_program(cmd, messageObject):
+
+    # Run the command
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    if process.returncode == EXIT_SUCCESS:
+        output = stdout
+    else:
+        output = stderr
+
+    # Send output to the message Object's channel
+    await messageObject.channel.send((output.decode("utf-8").strip("./")))
+        
 
 @client.event
 async def on_ready():
@@ -24,17 +41,20 @@ async def on_message(message):
         if message.channel.type == discord.ChannelType.private:
             print("Private message received")
             
-            # Create a unix-like arguments list using shlex
+            # Create a unix-like arguments list
             args = ["./enigma.out"] + shlex.split(message.content)
             
-            # Run engima executable with arguments
-            output = subprocess.check_output(args) #glob.glob("./" + "enigma.*")[0], message.content)
-
-            # Send executable output to the channel the message was received from
-            await message.channel.send((output.decode("utf-8").strip("./")))
+            await run_program(args, message)
 
         else:
-            if message.content.startswith("!"):
+        
+            if message.content.startswith("!enigma "):
+
                 print("Command received")
+            
+                # Create a unix-like arguments list (ignoring the command prefix)
+                args = ["./enigma.out"] + shlex.split(message.content)[1:]
+               
+                await run_program(args, message)
 
 client.run(token)

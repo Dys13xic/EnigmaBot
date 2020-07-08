740a12c4710a1355b4623cde46874524a09a61a3
Dys13xic <campbell.leckie@hotmail.com>
2020-07-08 19:13:43 -0400

    Bot sends a welcome message with usage info on server join

--- bot.py
+++ bot.py
@@ -6,12 +6,14 @@ import glob
 import shlex
 
 EXIT_SUCCESS = 0
+COMMAND_PREFIX = "!enigma "
 
 # Retrieve the discord bot token
 tokenFile = open("token.txt", 'r')
 token = tokenFile.readline()
 
 client = discord.Client()
+
 
 async def run_program(cmd, messageObject):
 
@@ -25,16 +27,30 @@ async def run_program(cmd, messageObject):
         output = stderr
 
     # Send output to the message Object's channel
-    await messageObject.channel.send((output.decode("utf-8").strip("./")))
-        
+    await messageObject.channel.send("```" + (output.decode("utf-8").strip("./")) + "```")
+
 
 @client.event
 async def on_ready():
     print("{} has connected to Discord".format(client.user))
 
+
+@client.event
+async def on_guild_join(guild):
+    welcomeMessage = """Hello, I am ENIGMA. I simulate the function of a Wehrmacht ENIGMA I cipher machine.
+`-` My command prefix is `!enigma`
+`-` Run a command by private message or command prefix
+`-` To see usage and options information, type `!enigma -h`"""
+
+    for channel in guild.text_channels:
+        if channel.permissions_for(guild.me).send_messages:
+
+            await channel.send(welcomeMessage)
+            break
+
+
 @client.event
 async def on_message(message):
-
     # Don't trigger on bot's sent messages
     if message.author != client.user:
 
@@ -47,11 +63,9 @@ async def on_message(message):
             await run_program(args, message)
 
         else:
-        
-            if message.content.startswith("!enigma "):
-
+            if message.content.startswith(COMMAND_PREFIX):
                 print("Command received")
-            
+              
                 # Create a unix-like arguments list (ignoring the command prefix)
                 args = ["./enigma.out"] + shlex.split(message.content)[1:]
                

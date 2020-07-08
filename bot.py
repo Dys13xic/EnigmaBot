81cc0136f42f93322ea688d0c0dd32305499c62f
Dys13xic <campbell.leckie@hotmail.com>
2020-07-07 22:14:21 -0400

    Started public channel command handling

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import subprocess
 import glob
 import shlex
 
+# Retrieve the discord bot token
 tokenFile = open("token.txt", 'r')
 token = tokenFile.readline()
 
@@ -16,14 +17,24 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
+    # Don't trigger on bot's sent messages
     if message.author != client.user:
 
         if message.channel.type == discord.ChannelType.private:
             print("Private message received")
+            
+            # Create a unix-like arguments list using shlex
             args = ["./enigma.out"] + shlex.split(message.content)
+            
+            # Run engima executable with arguments
             output = subprocess.check_output(args) #glob.glob("./" + "enigma.*")[0], message.content)
+
+            # Send executable output to the channel the message was received from
             await message.channel.send((output.decode("utf-8").strip("./")))
+
         else:
-            print("Public message receieved")
+            if message.content.startswith("!"):
+                print("Command received")
 
 client.run(token)
